5e9df1d07f637475482c5a63b63067c81c0df7c6
Ashwin Venkatesh <avenkatesh@pivotal.io>
2017-10-05 13:58:38 -0400

    Modified a couple fo things.
    [#148850469] Extract encryption values according to #6 approach in the db-refactor doc
    
    Signed-off-by: Iryna Shustava <ishustava@pivotal.io>

--- src/main/java/db/migration/common/V44_2__migrate_encypted_values_to_encryped_value_table.java
+++ src/main/java/db/migration/common/V44_2__migrate_encypted_values_to_encryped_value_table.java
@@ -4,7 +4,6 @@ import io.pivotal.security.util.UuidUtil;
 import org.flywaydb.core.api.migration.spring.SpringJdbcMigration;
 import org.springframework.jdbc.core.JdbcTemplate;
 
-import java.nio.ByteBuffer;
 import java.sql.Types;
 import java.util.List;
 import java.util.UUID;
@@ -18,17 +17,9 @@ public class V44_2__migrate_encypted_values_to_encryped_value_table implements
     String databaseName = jdbcTemplate.getDataSource().getConnection().getMetaData()
         .getDatabaseProductName().toLowerCase();
 
-    List<UUID> credentialsWithEncryptedValues = jdbcTemplate.query("select uuid from credential_version where encrypted_value is not null", (rowSet, rowNum) -> {
-      byte[] uuidBytes = rowSet.getBytes("uuid");
-      if (databaseName.equals("postgresql")) {
-        return UUID.fromString(new String(uuidBytes));
-      } else {
-        ByteBuffer byteBuffer = ByteBuffer.wrap(uuidBytes);
-        return new UUID(byteBuffer.getLong(), byteBuffer.getLong());
-      }
-    });
+    List<Object> credentialsWithEncryptedValues = jdbcTemplate.queryForList("select uuid from credential_version where encrypted_value is not null", Object.class);
 
-    for (UUID credentialUuid : credentialsWithEncryptedValues) {
+    for (Object credentialUuid : credentialsWithEncryptedValues) {
       Object encryptedValueUuid = makeUuid(databaseName);
       jdbcTemplate.update(
           "insert into encrypted_value (" +
@@ -46,44 +37,46 @@ public class V44_2__migrate_encypted_values_to_encryped_value_table implements
               "updated_at "+
               "from credential_version " +
               "where uuid = ?",
-          new Object[]{encryptedValueUuid, getUuidParam(databaseName, credentialUuid)},
+          new Object[]{encryptedValueUuid, credentialUuid},
           new int[]{Types.VARBINARY, Types.VARBINARY});
       jdbcTemplate.update(
           "update credential_version " +
               "set encrypted_value_uuid = ? " +
               "where uuid = ?",
-          new Object[]{encryptedValueUuid, getUuidParam(databaseName, credentialUuid)},
+          new Object[]{encryptedValueUuid, credentialUuid},
           new int[]{Types.VARBINARY, Types.VARBINARY});
     }
 
-//    List<UUID> passwordsWithEncryptedValues = jdbcTemplate.query("select uuid from password_credential where encrypted_generation_parameters is not null", (rowSet, rowNum) -> {
-//      byte[] uuidBytes = rowSet.getBytes("uuid");
-//      if (databaseName.equals("postgresql")) {
-//        return UUID.fromString(new String(uuidBytes));
-//      } else {
-//        ByteBuffer byteBuffer = ByteBuffer.wrap(uuidBytes);
-//        return new UUID(byteBuffer.getLong(), byteBuffer.getLong());
-//      }
-//    });
-//
-//    for (UUID record : passwordsWithEncryptedValues) {
-//      Object uuid = makeUuid(databaseName);
-//      jdbcTemplate.update(
-//          "insert into encrypted_value values " +
-//              "(select ?, credential_version.encryption_key_uuid, " +
-//              "password_credential.encrypted_generation_parameters, "  +
-//              "password_credential.parameters_nonce, credential_version.updated_at)" +
-//              "from credential_version, password_credential " +
-//              "where credential_version.uuid=password_credential.uuid and " +
-//              "credential_version.uuid is ?);",
-//          uuid, record);
-//      jdbcTemplate.update(
-//          "update password_credential set password_parameters_uuid=? " +
-//              "where uuid = ?",
-//          uuid, record);
-//    }
-//
-//
+    List<Object> passwordsWithEncryptedValues = jdbcTemplate.queryForList("select uuid from password_credential where encrypted_generation_parameters is not null", Object.class);
+
+    for (Object passwordCredentialUuid : passwordsWithEncryptedValues) {
+      Object encryptedValueUuid = makeUuid(databaseName);
+      jdbcTemplate.update(
+          "insert into encrypted_value (" +
+              "uuid, " +
+              "encryption_key_uuid, " +
+              "encrypted_value, " +
+              "nonce, " +
+              "updated_at" +
+              ")" +
+              "select " +
+              "?, " +
+              "credential_version.encryption_key_uuid, " +
+              "password_credential.encrypted_generation_parameters, "  +
+              "password_credential.parameters_nonce, " +
+              "credential_version.updated_at" +
+              "from credential_version, password_credential " +
+              "where credential_version.uuid=password_credential.uuid and " +
+              "credential_version.uuid = ?);",
+          new Object[]{encryptedValueUuid, passwordCredentialUuid},
+          new int[]{Types.VARBINARY, Types.VARBINARY});
+      jdbcTemplate.update(
+          "update password_credential set password_parameters_uuid=? " +
+              "where uuid = ?",
+          encryptedValueUuid, passwordCredentialUuid);
+    }
+
+
 //    List<UUID> userCredentialsWithEncryptedValues = jdbcTemplate.queryForList("select uuid " +
 //        "from user_credential where encrypted_generation_parameters is not null", UUID.class);
 //
